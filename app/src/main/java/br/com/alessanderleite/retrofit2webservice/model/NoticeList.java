863242432d807396c44e649dd047ad0e8d949171
alessanderleite <alessander.logistica@gmail.com>
2019-04-08 22:35:52 -0300

    code the NoticeList class

---
+++ app/src/main/java/br/com/alessanderleite/retrofit2webservice/model/NoticeList.java
@@ -0,0 +1,19 @@
+package br.com.alessanderleite.retrofit2webservice.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+public class NoticeList {
+
+    @SerializedName("notice_list")
+    private ArrayList<Notice> noticeList;
+
+    public ArrayList<Notice> getNoticeList() {
+        return noticeList;
+    }
+
+    public void setNoticeList(ArrayList<Notice> noticeList) {
+        this.noticeList = noticeList;
+    }
+}
