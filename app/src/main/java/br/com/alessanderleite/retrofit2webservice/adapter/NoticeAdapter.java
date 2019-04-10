d5a60ccabfb6ea8701a59dd6da8721e33a2e1796
alessanderleite <alessander.logistica@gmail.com>
2019-04-09 22:23:15 -0300

    create the NoticeAdapter class

---
+++ app/src/main/java/br/com/alessanderleite/retrofit2webservice/adapter/NoticeAdapter.java
@@ -0,0 +1,52 @@
+package br.com.alessanderleite.retrofit2webservice.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import br.com.alessanderleite.retrofit2webservice.R;
+import br.com.alessanderleite.retrofit2webservice.model.Notice;
+
+public class NoticeAdapter extends RecyclerView.Adapter<NoticeAdapter.NoticeViewHolder> {
+
+    private ArrayList<Notice> dataList;
+
+    public NoticeAdapter(ArrayList<Notice> dataList) {
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public NoticeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.single_row_view, parent, false);
+        return new NoticeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NoticeViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public class NoticeViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtNoticeTitle, txtNoticeBrif, txtNoticeFilePath;
+
+        public NoticeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txtNoticeTitle = itemView.findViewById(R.id.txt_notice_title);
+            txtNoticeBrif = itemView.findViewById(R.id.txt_notice_brief);
+            txtNoticeFilePath = itemView.findViewById(R.id.txt_notice_file_path);
+        }
+    }
+}
